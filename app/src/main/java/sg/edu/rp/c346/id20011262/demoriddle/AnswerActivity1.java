cb40d229e8eceab0623e4a14d0d0a83e1e198f71
FOO JING XIANG <20011262@myrp.edu.sg>
2021-05-24 21:09:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011262/demoriddle/AnswerActivity1.java
@@ -0,0 +1,61 @@
+package sg.edu.rp.c346.id20011262.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class AnswerActivity1 extends AppCompatActivity {
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer1);
+        Log.d("AnswerActivity1", "onCreate() called.");
+
+        Intent intentReceive = getIntent();
+        String questionSelected = intentReceive.getStringExtra("Question");
+
+        tvAnswer = findViewById(R.id.textView);
+        if(questionSelected.equalsIgnoreCase("q1")) {
+            tvAnswer.setText(questionSelected + " answer is : Queue");
+        } else if (questionSelected.equalsIgnoreCase("q2")){
+            tvAnswer.setText(questionSelected + " answer is: Gone");
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("AnswerActivity1", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("AnswerActivity1", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("AnswerActivity1", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("AnswerActivity1", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("AnswerActivity1", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("AnswerActivity1", "onRestart() called.");
+        super.onRestart();
+    }
+}
